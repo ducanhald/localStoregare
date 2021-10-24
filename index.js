a0a185be6d2b8e9be47447ac3040dd8d4d5b6598
ducanhald <ducanhald@gmail.com>
2021-10-24 21:02:27 +0700

    local storegare

---
+++ index.js
@@ -0,0 +1,22 @@
+function createStorage(key) {
+  const store = JSON.parse(localStorage.getItem(key)) ?? {};
+  const save = () => {
+    localStorage.setItem(key, JSON.stringify(store));
+  };
+  const storage = {
+    get(key) {
+      return store[key];
+    },
+    set(key, value) {
+      store[key] = value;
+      save();
+    },
+    remove(key) {
+      delete store[key];
+      save();
+    },
+  };
+  return storage;
+}
+const ProfileSettings = createStorage("profile-setting");
+console.log(ProfileSettings.remove("fullName"));
